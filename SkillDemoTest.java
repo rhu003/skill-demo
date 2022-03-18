9ed79268f58957a21aea247eab2bb0cea19c143d
rhu003 <rhu@ucsd.edu>
2022-03-18 10:34:17 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiplication() {
-        assertEquals(3, SkillDemo.multiply(1, 2));
+        assertEquals(3, SkillDemo.multiply(1, 3));
     }
 }
